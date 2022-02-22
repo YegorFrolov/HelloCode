65cb780ccc21eb53a246e8491cf023a073bdc573
YegorFrolov <dmtyegor@gmail.com>
2022-02-22 23:44:22 +0300

    Рандом в 3м примере

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿duble numberA = 10;
+duble numberB = 5;
+Console.WriteLine(numberA / numberB); 
